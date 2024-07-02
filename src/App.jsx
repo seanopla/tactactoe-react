e26b4c07452a756e9004f7b3231228a7705bf791
naufal <ahmadnaufalez@gmail.com>
2024-07-02 11:23:58 +0700

    "Added CSS styles and modified App component to render a Tic-Tac-Toe game board and game info section"

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,39 @@
-import { useState } from "react"
-import reactLogo from "./assets/react.svg"
-import viteLogo from "/vite.svg"
 import "./App.css"
 
+const Button = ({ children }) => {
+  return <button className="button">{children}</button>
+}
+
 function App() {
-  return <h1>Hello World</h1>
+  return (
+    <div className="container">
+      <div className="board">
+        <div className="row">
+          <div className="square">1</div>
+          <div className="square">2</div>
+          <div className="square">3</div>
+        </div>
+        <div className="row">
+          <div className="square">4</div>
+          <div className="square">5</div>
+          <div className="square">6</div>
+        </div>
+        <div className="row">
+          <div className="square">7</div>
+          <div className="square">8</div>
+          <div className="square">9</div>
+        </div>
+      </div>
+      <div className="game-info">
+        <h2>Next Player: X</h2>
+        <Button>Start Over</Button>
+        <h2>History</h2>
+        <Button>Langkah 1</Button>
+        <Button>Langkah 2</Button>
+        <Button>Langkah 3</Button>
+      </div>
+    </div>
+  )
 }
 
 export default App
