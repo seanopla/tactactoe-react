6b96ebd13e1d342bdcdba1bd54e2228711fc2cef
naufal <ahmadnaufalez@gmail.com>
2024-07-02 11:42:35 +0700

    "Extracted Square component from App, added state and click handler to Square"

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
 import "./App.css"
+import { useState } from "react"
 
 const Button = ({ children }) => {
   return <button className="button">{children}</button>
+}
+
+const Square = ({ value }) => {
+  const [state, setState] = useState(null)
+
+  const handleClick = () => {
+    setState("X")
+    console.log("CLICKED SQUARE NUMBER", value)
+  }
+
+  return (
+    <div className="square" onClick={handleClick}>
+      {state}
+    </div>
+  )
 }
 
 function App() {
@@ -9,19 +25,19 @@ function App() {
     <div className="container">
       <div className="board">
         <div className="row">
-          <div className="square">1</div>
-          <div className="square">2</div>
-          <div className="square">3</div>
+          <Square value={1} />
+          <Square value={2} />
+          <Square value={3} />
         </div>
         <div className="row">
-          <div className="square">4</div>
-          <div className="square">5</div>
-          <div className="square">6</div>
+          <Square value={4} />
+          <Square value={5} />
+          <Square value={6} />
         </div>
         <div className="row">
-          <div className="square">7</div>
-          <div className="square">8</div>
-          <div className="square">9</div>
+          <Square value={7} />
+          <Square value={8} />
+          <Square value={9} />
         </div>
       </div>
       <div className="game-info">
