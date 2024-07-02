2f2c1f2c0597538f0ea788cda9dee2038c46c301
naufal <ahmadnaufalez@gmail.com>
2024-07-02 13:08:49 +0700

    "Updated App component to manage game state with a board array and toggle between X and O"

--- src/App.jsx
+++ src/App.jsx
@@ -15,8 +15,21 @@ const Square = ({ value, onClick }) => {
 
 function App() {
   const [isXNext, setIsXNext] = useState(false)
-  const handleSquareClick = () => {
-    setIsXNext(!isXNext)
+  const [board, setBoard] = useState(Array(9).fill(null))
+
+  const handleSquareClick = (index) => {
+    setBoard((currBoard) => {
+      const newBoard = [...currBoard]
+
+      if (newBoard[index]) {
+        return newBoard
+      }
+
+      newBoard[index] = isXNext ? "X" : "O"
+
+      setIsXNext(!isXNext)
+      return newBoard
+    })
     console.log("CLICKED SQUARE NUMBER")
   }
 
@@ -24,19 +37,19 @@ function App() {
     <div className="container">
       <div className="board">
         <div className="row">
-          <Square value={1} onClick={handleSquareClick} />
-          <Square value={2} onClick={handleSquareClick} />
-          <Square value={3} onClick={handleSquareClick} />
+          <Square value={board[0]} onClick={() => handleSquareClick(0)} />
+          <Square value={board[1]} onClick={() => handleSquareClick(1)} />
+          <Square value={board[2]} onClick={() => handleSquareClick(2)} />
         </div>
         <div className="row">
-          <Square value={4} onClick={handleSquareClick} />
-          <Square value={5} onClick={handleSquareClick} />
-          <Square value={6} onClick={handleSquareClick} />
+          <Square value={board[3]} onClick={() => handleSquareClick(3)} />
+          <Square value={board[4]} onClick={() => handleSquareClick(4)} />
+          <Square value={board[5]} onClick={() => handleSquareClick(5)} />
         </div>
         <div className="row">
-          <Square value={7} onClick={handleSquareClick} />
-          <Square value={8} onClick={handleSquareClick} />
-          <Square value={9} onClick={handleSquareClick} />
+          <Square value={board[6]} onClick={() => handleSquareClick(6)} />
+          <Square value={board[7]} onClick={() => handleSquareClick(7)} />
+          <Square value={board[8]} onClick={() => handleSquareClick(8)} />
         </div>
       </div>
       <div className="game-info">
